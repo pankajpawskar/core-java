1a7fe035cf97b2d4061ac7f3461c0cac43be63da
pankajpawskar <pankaj9xworkz.com>
2022-08-12 12:19:37 +0530

    dataType

---
+++ Wallet.java
@@ -0,0 +1,20 @@
+ class Wallet{
+	 static void brand(String brand){
+	  System.out.println("BrandName = "+brand);
+	 }
+	 static void colour(String colour1, String colour2 ){
+		System.out.println("Colour of the wallet "+ colour1);
+		System.out.println("Colour of the wallet "+ colour2);
+	 }
+	 static void specifications(String material, boolean cardholder, int pockets){
+		System.out.println("Material : "+material+","+ " cardholder is found :"+cardholder+","+" No of pockets : "+pockets);
+		
+	 }
+	
+	 static void  size(int lenght,int width,int thinckness){
+       System.out.println("Wallet dimension : "+lenght+"X"+width+"X"+thinckness);
+ }
+	 
+	 
+	 
+ }
